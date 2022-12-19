b2897ca37f88e59b5873a9f3d1ec13693668d8ef
Tiare EA <106929329+Tiarethorns@users.noreply.github.com>
2022-12-19 14:44:57 -0300

    Add files via upload

---
+++ FRONT-END/CURSO-HTML/JAVASCRIPT/const tiare=[.js
@@ -0,0 +1,13 @@
+const tiare = [
+  (nombre = "Tiare Espinoza"),
+  (edad = 34),
+  (isDev = true),
+  (cumpleaños = new Date("june 20 1988")),
+  (libro = {
+    autor: "hermann hesse",
+    titulo: "Damian",
+    fecha: new Date("april 5 2020"),
+    url: "http://biblio3.url.edu.gt/Libros/2011/Demian.pdf",
+  }),
+];
+console.log(tiare);
